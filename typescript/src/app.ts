76f6719bccf2bd290aa7083eb62636cd65214d51
feifel <roman.buchser@gmail.com>
2024-02-12 02:09:21 +0100

    Add color picker in TypeScript
    Add color picker in TypeScript and WebXR

--- typescript/src/app.ts
+++ typescript/src/app.ts
@@ -1,8 +1,10 @@
 import "@babylonjs/core/Debug/debugLayer";
 import "@babylonjs/inspector";
-import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder, FreeCamera } from "@babylonjs/core";
+import { Engine, Scene, Vector3, HemisphericLight, Mesh, MeshBuilder, FreeCamera, StandardMaterial } from "@babylonjs/core";
+import { AdvancedDynamicTexture, StackPanel, Control, TextBlock, ColorPicker} from "@babylonjs/gui";
 
 class App {
+    public scene: Scene;
     constructor() {
         // create the canvas html element and attach it to the webpage
         var canvas = document.createElement("canvas");
@@ -13,10 +15,10 @@ class App {
 
         // initialize babylon scene and engine
         var engine = new Engine(canvas, true);
-        var scene = new Scene(engine);
+        this.scene = new Scene(engine);
 
         // This creates and positions a free camera (non-mesh)
-        var camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
+        var camera = new FreeCamera("camera1", new Vector3(0, 5, -10), this.scene);
 
         // This targets the camera to scene origin
         camera.setTarget(Vector3.Zero());
@@ -25,36 +27,68 @@ class App {
         camera.attachControl(canvas, true);
 
         // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-        var light = new HemisphericLight("light1", new Vector3(0, 1, 0), scene);
+        var light = new HemisphericLight("light1", new Vector3(0, 1, 0), this.scene);
 
         // Default intensity is 1. Let's dim the light a small amount
         light.intensity = 0.7;
 
         // Our built-in 'sphere' shape. Params: name, options, scene
-        var sphere = MeshBuilder.CreateSphere("sphere", {diameter: 2, segments: 32}, scene);
+        var sphere = MeshBuilder.CreateSphere("sphere", {diameter: 2, segments: 32}, this.scene);
 
         // Move the sphere upward 1/2 its height
         sphere.position.y = 1;
 
-        // Our built-in 'ground' shape. Params: name, options, scene
-        var ground = MeshBuilder.CreateGround("ground", {width: 6, height: 6}, scene);
+        sphere.material = new StandardMaterial("sphereMat", this.scene);
+            
+        // GUI
+        var plane = Mesh.CreatePlane("plane", 1, this.scene);
+        plane.position = new Vector3(1.4, 1.5, 0.4)
+        var advancedTexture = AdvancedDynamicTexture.CreateForMesh(plane);
+        var panel = new StackPanel();    
+        advancedTexture.addControl(panel);  
+        var header = new TextBlock();
+        header.text = "Color GUI";
+        header.height = "100px";
+        header.color = "white";
+        header.textHorizontalAlignment = Control.HORIZONTAL_ALIGNMENT_CENTER;
+        header.fontSize = "120"
+        panel.addControl(header); 
+        var picker = new ColorPicker();
+        var material = <StandardMaterial>sphere.material;
+        picker.value = material.diffuseColor;
+        picker.horizontalAlignment = Control.HORIZONTAL_ALIGNMENT_CENTER;
+        picker.height = "350px";
+        picker.width = "350px";
+        picker.onValueChangedObservable.add(function(value) {
+            material.diffuseColor.copyFrom(value);
+        });
+        panel.addControl(picker);
+    
 
         // hide/show the Inspector
         window.addEventListener("keydown", (ev) => {
             // Shift+Ctrl+Alt+I
             if (ev.shiftKey && ev.ctrlKey && ev.altKey && ev.keyCode === 73) {
-                if (scene.debugLayer.isVisible()) {
-                    scene.debugLayer.hide();
+                if (this.scene.debugLayer.isVisible()) {
+                    this.scene.debugLayer.hide();
                 } else {
-                    scene.debugLayer.show();
+                    this.scene.debugLayer.show();
                 }
             }
         });
 
         // run the main render loop
         engine.runRenderLoop(() => {
-            scene.render();
+            this.scene.render();
+        });
+    }
+    async createDefaultXRExperienceAsync() {        
+        const environment = this.scene.createDefaultEnvironment();
+        // XR
+        const xrHelper = await this.scene.createDefaultXRExperienceAsync({
+            floorMeshes: [environment.ground]
         });
     }
 }
-new App();
+var app = new App();
+app.createDefaultXRExperienceAsync();
