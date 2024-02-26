208181135cc702f0895ea66698d588ef64f6d51c
feifel <roman.buchser@gmail.com>
2024-02-26 01:21:11 +0100

    Configure sass and apply cyan theme in pico.css

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import '@picocss/pico'
+    import Hamburger from '$lib/components/Hamburger.svelte'
+    import Expander from '$lib/components/Expander.svelte'
+</script>
+<style lang="scss">
+    @use "@picocss/pico/scss/pico" with (
+        $theme-color: "cyan"
+    );
+</style>
+<nav style="margin-left:25px; margin-right:25px">
+    <ul><li><a href="#">Menu</a></li></ul>
+    <ul><li style="font-Size:36px; color:aqua;"><b>WebXR Firework Simulator</b></li></ul>
+    <ul><li><a href="#">Edit</a></li></ul>
+</nav>
+<slot />
--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,15 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	define: {
 		'process.env.NODE_ENV': '"production"',
+	},
+	css: {
+		preprocessorOptions: {
+		  sass: {
+			additionalData: `
+			  @import '$lib/sass/variables'
+			  @import '$lib/sass/mixins'
+			`,
+		  }
+		},
 	}
 });
