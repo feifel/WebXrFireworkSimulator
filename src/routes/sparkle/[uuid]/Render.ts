90e5795a851d93471dafcbce780a51b816048266
feifel <roman.buchser@gmail.com>
2024-04-01 15:15:43 +0200

    Implement first particle system

--- src/routes/sparkle/[uuid]/Render.ts
+++ src/routes/sparkle/[uuid]/Render.ts
@@ -1,6 +1,6 @@
 import "@babylonjs/core/Debug/debugLayer";
 import "@babylonjs/inspector";
-import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder, FreeCamera } from "@babylonjs/core";
+import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder, FreeCamera, ParticleSystem, Texture, Color4, PointLight, StandardMaterial, Color3 } from "@babylonjs/core";
 
 export default class Render {
     canvas: HTMLCanvasElement
@@ -13,30 +13,48 @@ export default class Render {
     }
 
     setup(): void {
-        // This creates and positions a free camera (non-mesh)
-        var camera = new FreeCamera("camera1", new Vector3(0, 5, -10), this.scene);
-
-        // This targets the camera to scene origin
-        camera.setTarget(Vector3.Zero());
-
-        // This attaches the camera to the canvas
+        // Setup environment
+        var camera = new ArcRotateCamera("ArcRotateCamera", 1, 1, 6, new Vector3(0, 0, 0), this.scene);
         camera.attachControl(this.canvas, true);
 
-        // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-        var light = new HemisphericLight("light1", new Vector3(0, 1, 0), this.scene);
+        // Create a particle system
+        var particleSystem = new ParticleSystem("particles", 2000, this.scene);
 
-        // Default intensity is 1. Let's dim the light a small amount
-        light.intensity = 0.7;
+        //Texture of each particle
+        particleSystem.particleTexture = new Texture("/textures/flare.png", this.scene);
 
-        // Our built-in 'sphere' shape. Params: name, options, scene
-        var sphere = MeshBuilder.CreateSphere("sphere", {diameter: 2, segments: 32}, this.scene);
+        // Where the particles come from
+        particleSystem.emitter = Vector3.Zero(); // the starting location
 
-        // Move the sphere upward 1/2 its height
-        sphere.position.y = 1;
+        // Colors of all particles
+        particleSystem.color1 = new Color4(0.99, 0.9, 0.08);
+        particleSystem.color2 = new Color4(0.69, 0.22, 0.08);
+        particleSystem.colorDead = new Color4(0.2, 0, 0, 0);
 
-        // Our built-in 'ground' shape. Params: name, options, scene
-        var ground = MeshBuilder.CreateGround("ground", {width: 6, height: 6}, this.scene);
+        // Set the gravity of all particles
+        particleSystem.gravity = new Vector3(0, -9.81, 0);
 
+        // Size of each particle (random between...
+        particleSystem.minSize = 0.005;
+        particleSystem.maxSize = 0.05;
+
+        // Life time of each particle (random between...
+        particleSystem.minLifeTime = 0.3;
+        particleSystem.maxLifeTime = 1.0;
+
+        // Emission rate
+        particleSystem.emitRate = 10000;
+
+        /******* Emission Space ********/
+        particleSystem.createPointEmitter(new Vector3(-3, 8, 3), new Vector3(3, 8, -3));
+
+        // Speed
+        particleSystem.minEmitPower = 0.4;
+        particleSystem.maxEmitPower = 0.6;
+        particleSystem.updateSpeed = 0.008;
+
+        // Start the particle system
+        particleSystem.start();
         // hide/show the Inspector
         window.addEventListener("keydown", (ev) => {
             // Shift+Ctrl+Alt+I
@@ -54,5 +72,4 @@ export default class Render {
 	run(): void {
 		this.engine.runRenderLoop(() => this.scene.render());
 	}
-
 }
